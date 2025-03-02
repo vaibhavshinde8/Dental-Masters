a68e025ddabbf238f6eb51e91f08961ac0fa5e85
VAIBHAV DEEPAK SHINDE <123886126+vaibhavshinde8@users.noreply.github.com>
2025-03-02 12:00:00 +0530

    Added Our Team Images

--- src/Pages/OurTeam.jsx
+++ src/Pages/OurTeam.jsx
@@ -39,7 +39,7 @@ const OurTeam = () => {
           className="w-full h-96 object-cover opacity-50"
           alt="Dental Office"
         />
-        {/* Dark overlay */}
+       
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40"></div>
 
         {/* Heading */}
